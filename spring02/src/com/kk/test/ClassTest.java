611502a674656390ce9d6248269af588599c8c05
kaige.yuan <y13262639708@163.com>
2021-11-12 20:27:33 +0800

    基于xml文件完成对象的创建和属性的注入（使用有参构造完成注入）

--- spring02/src/com/kk/test/ClassTest.java
+++ spring02/src/com/kk/test/ClassTest.java
@@ -1,6 +1,7 @@
 package com.kk.test;
 
 import com.kk.java.Book;
+import com.kk.java.Order;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -12,5 +13,12 @@ public class ClassTest {
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         Book book = context.getBean("book", Book.class);
         System.out.println(book);
+    }
+
+    @Test
+    public void test2() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Order order = context.getBean("order", Order.class);
+        System.out.println(order);
     }
 }
