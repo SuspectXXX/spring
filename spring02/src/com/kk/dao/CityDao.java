4979d3dbb16caf72511f6ed7a015dd4ec00881ff
kaige.yuan <y13262639708@163.com>
2021-11-13 21:08:30 +0800

    基于xml文件完成级联赋值

--- spring02/src/com/kk/dao/CityDao.java
+++ spring02/src/com/kk/dao/CityDao.java
@@ -3,6 +3,6 @@ package com.kk.dao;
 public class CityDao {
 
     public void update() {
-        System.out.println("CityDao update...");
+        System.out.println("CityDao update......");
     }
 }
