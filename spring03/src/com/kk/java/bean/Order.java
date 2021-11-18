de4d705b9556a7f1ac24f366ec3b3d2dc17db0c4
kaige.yuan <y13262639708@163.com>
2021-11-18 22:40:43 +0800

    基于注解完成bean的注入以及属性的注入

--- spring03/src/com/kk/java/bean/Order.java
+++ spring03/src/com/kk/java/bean/Order.java
@@ -1,7 +1,39 @@
 package com.kk.java.bean;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
 
 @Component
 public class Order {
+
+    @Autowired
+    private Order1 order1;
+
+    @Autowired
+    @Qualifier(value = "order2")
+    private Order2 order2;
+
+    @Resource
+    private Order3 order3;
+
+    @Value(value = "abc")
+    private String s;
+
+    @Value(value = "1")
+    private int oId;
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "order1=" + order1 +
+                ", order2=" + order2 +
+                ", order3=" + order3 +
+                ", s='" + s + '\'' +
+                ", oId=" + oId +
+                '}';
+    }
 }
