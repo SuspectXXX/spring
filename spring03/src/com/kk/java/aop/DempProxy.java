4c77c4931b0b727caffe8d76614bd07e4d49e4b1
kaige.yuan <y13262639708@163.com>
2021-12-02 15:13:51 +0800

    测试AOP中的各个切面

--- spring03/src/com/kk/java/aop/DempProxy.java
+++ spring03/src/com/kk/java/aop/DempProxy.java
@@ -1,8 +1,8 @@
 package com.kk.java.aop;
 
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,15 +10,33 @@ import org.springframework.stereotype.Component;
 public class DempProxy {
 
     //前置通知
-    @Before("execution(public * com.kk.java.aop.Demo.demo(..))")
-    public void before() {
+    @Before(value="execution(public * com.kk.java.aop.Demo.demo(..))")
+    public void before(JoinPoint joinPoint) {
+        System.out.println("====="+joinPoint.getSignature().getName());
         System.out.println("before...");
     }
 
     //最终通知
-    @After("execution(void com.kk.java.aop.Demo.*())")
+    @After(value="execution(void com.kk.java.aop.Demo.*())")
     public void after() {
         System.out.println("after...");
+    }
+
+    //后置通知
+    @AfterReturning(value="execution(void com.kk.java.aop.Demo.*())", returning="result")
+    public void afterReturning(JoinPoint joinPoint, Object result) {
+        System.out.println("after...");
+    }
+
+    //异常通知
+    @AfterThrowing(value="execution(void com.kk.java.aop.Demo.*())", throwing="ex")
+    public void afterThrowing(JoinPoint joinPoint, Exception ex) {
+        System.out.println("after...");
+    }
+
+    @Around(value = "execution(* * com.kk.java.aop.Demo.*(..))")
+    public void around(ProceedingJoinPoint proceedingJoinPoint) {
+
     }
 
 }
